d1974d2d32672598da5d6b0d4bd96f1d5072f427
Koffi Nyonouglo <nyonouglokoffi8@gmail.com>
2020-08-24 06:14:19 +0000

    In app Media player
    This is to demonstrate how
    to use windows media player in your app.

---
+++ Windows_player_se/Windows_player/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Windows_player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+       this.axWindowsMediaPlayer1.URL = "E:\\titus\\daysofRedemption.mp4";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
